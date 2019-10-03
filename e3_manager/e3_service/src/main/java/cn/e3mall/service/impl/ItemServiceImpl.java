358d9523a010f2f51427ccc7ebc093b8e14b7f6c
林泽豪 <1064292818@qq.com>
2019-10-03 20:43:44 +0800

    e3mall搭建环境

---
+++ e3_manager/e3_service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -0,0 +1,29 @@
+package cn.e3mall.service.impl;
+
+import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
+import cn.e3mall.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private TbItemMapper tbItemMapper;
+
+    @Override
+    public TbItem getItemById(Long itemId) {
+        TbItemExample tbItemExample = new TbItemExample();
+        TbItemExample.Criteria criteria = tbItemExample.createCriteria();
+        criteria.andIdEqualTo(itemId);
+        List<TbItem> tbItems = tbItemMapper.selectByExample(tbItemExample);
+        if (null != tbItems && tbItems.size() > 0){
+            return tbItems.get(0);
+        }
+        return null;
+    }
+}
